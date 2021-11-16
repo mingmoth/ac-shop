e973499a51daa02bd4bf3d37bb3c0307adb4a88f
mingmoth <mingmoth@gmail.com>
2021-11-16 21:57:56 +0800

    project init

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '*',
+      name: 'Not-Found',
+      component: NotFound
+    },
+  ]
+})
---
+++ src/views/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>404 Page Not FOund</h1>
+</template>
