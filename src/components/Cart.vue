65cbffcdb54ac05133fd2f4d999f0943e54ef44a
mingmoth <mingmoth@gmail.com>
2021-11-17 21:40:01 +0800

    add Cart.vue & related css

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="cart">
     <div class="cart-title">購物籃</div>
+    <div class="cart-item">
+      <div class="cart-item-img" id="jean1"></div>
+      <div class="cart-item-body">
+        <div class="cart-item-body-head">
+          <div class="cart-item-body-head-name">破壞補丁修身牛仔褲</div>
+          <div class="cart-item-body-head-count">
+            <button class="btn btn-minus">-</button>
+            <div class="cart-item-body-head-count-num">1</div>
+            <button class="btn btn-plus">+</button>
+          </div>
+        </div>
+        <div class="cart-item-body-price">$3.999</div>
+      </div>
+    </div>
+    <div class="cart-item">
+      <div class="cart-item-img" id="jean2"></div>
+      <div class="cart-item-body">
+        <div class="cart-item-body-head">
+          <div class="cart-item-body-head-name">刷色直筒牛仔褲</div>
+          <div class="cart-item-body-head-count">
+            <button class="btn btn-minus">-</button>
+            <div class="cart-item-body-head-count-num">1</div>
+            <button class="btn btn-plus">+</button>
+          </div>
+        </div>
+        <div class="cart-item-body-price">$1299</div>
+      </div>
+    </div>
+    <div class="cart-ship">
+      <div class="cart-ship-title">運費</div>
+      <div class="cart-ship-price">免費</div>
+    </div>
+    <div class="cart-total">
+      <div class="cart-total-title">小計</div>
+      <div class="cart-total-price">$5,298</div>
+    </div>
   </div>
 </template>
