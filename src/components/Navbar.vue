d3c304af87f55214fbd7e1882af282e7c68df335
mingmoth <mingmoth@gmail.com>
2021-11-19 22:09:57 +0800

    add rest Navbar function & css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,22 +6,37 @@
     <div class="nav-navbar">
       <div class="nav-navbar-select">
         <ul class="nav-navbar-select-list">
-          <li class="nav-navbar-select-list-link"><a href="">男款</a></li>
-          <li class="nav-navbar-select-list-link"><a href="">女款</a></li>
-          <li class="nav-navbar-select-list-link"><a href="">最新消息</a></li>
-          <li class="nav-navbar-select-list-link"><a href="">客製商品</a></li>
-          <li class="nav-navbar-select-list-link"><a href="">聯絡我們</a></li>
+          <li class="nav-navbar-select-list-link">男款</li>
+          <li class="nav-navbar-select-list-link">女款</li>
+          <li class="nav-navbar-select-list-link">最新消息</li>
+          <li class="nav-navbar-select-list-link">客製商品</li>
+          <li class="nav-navbar-select-list-link">聯絡我們</li>
         </ul>
       </div>
       <div class="nav-navbar-option">
         <div class="nav-navbar-option-wrapper">
-          <div class="nav-navbar-option-wrapper-item"></div>
-          <div class="nav-navbar-option-wrapper-item"></div>
-          <label for="darkmode" class="nav-navbar-option-wrapper-item"></label>
+          <div class="nav-navbar-option-wrapper-search"></div>
+          <div class="nav-navbar-option-wrapper-cart"></div>
+          <label for="darkmode" class="nav-navbar-option-wrapper-switch"></label>
           <input type="checkbox" class="darkmode"
-            id="darkmode">
+            id="darkmode" @change="darkToggle">
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Navbar",
+  methods: {
+    darkToggle(e) {
+      if (e.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+      } else {
+        document.documentElement.setAttribute("data-theme", "light");
+      }
+    },
+  }
+}
+</script>
