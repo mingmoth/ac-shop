06160ae815baa023e2cedd9d67ef1d7148079c29
mingmoth <mingmoth@gmail.com>
2021-11-19 15:08:09 +0800

    add Navbar.vue & partial css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Navbar />
     <router-view/>
   </div>
 </template>
@@ -7,8 +8,12 @@
 <script>
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar.vue'
 export default {
   name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,27 @@
 <template>
-  <div class="navbar"></div>
+  <div class="nav">
+    <div class="nav-logo"></div>
+    <label for="nav-toggle" class="nav-toggle"></label>
+    <input type="checkbox" name="nav-toggle" id="nav-toggle" />
+    <div class="nav-navbar">
+      <div class="nav-navbar-select">
+        <ul class="nav-navbar-select-list">
+          <li class="nav-navbar-select-list-link"><a href="">男款</a></li>
+          <li class="nav-navbar-select-list-link"><a href="">女款</a></li>
+          <li class="nav-navbar-select-list-link"><a href="">最新消息</a></li>
+          <li class="nav-navbar-select-list-link"><a href="">客製商品</a></li>
+          <li class="nav-navbar-select-list-link"><a href="">聯絡我們</a></li>
+        </ul>
+      </div>
+      <div class="nav-navbar-option">
+        <div class="nav-navbar-option-wrapper">
+          <div class="nav-navbar-option-wrapper-item"></div>
+          <div class="nav-navbar-option-wrapper-item"></div>
+          <label for="darkmode" class="nav-navbar-option-wrapper-item"></label>
+          <input type="checkbox" class="darkmode"
+            id="darkmode">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
